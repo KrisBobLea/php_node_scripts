c5a2b13e7d8e5d85d168fa43b0c8091345b5fed6
Mio Lau <liurusi.101@gmail.com>
2018-06-16 13:56:24 +0800

    [add] start docker service after system reboot

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 systemctl start docker
+systemctl enable docker
 
 # ------------------ install docker-compose ------------------
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
