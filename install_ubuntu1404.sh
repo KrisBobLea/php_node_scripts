3a6f6ae1dd9f1a5243905745cdc0a3856baea9b0
Mio Lau <liurusi.101@gmail.com>
2018-07-23 14:02:52 +0800

    [update] enable mod_rewrite

---
+++ install_ubuntu1404.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get remove -y docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# ------------------ start php node project ------------------
+docker run -itd --name mfpad -p 80:80 -e TZ=Asia/Shanghai --restart always --log-opt max-size=10m --log-opt max-file=10 mio101/php_node_scripts
--- install_v2ray.sh
+++ install_v2ray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p /etc/v2ray
-cp config.json /etc/v2ray/
+curl https://raw.githubusercontent.com/MioYvo/php_node_scripts/master/config.json -o /etc/v2ray/config.json
 docker run -itd --name v2ray --restart always -p 31560:31560 -p 31561:31561 -v /etc/v2ray/:/etc/v2ray/ v2ray/official
