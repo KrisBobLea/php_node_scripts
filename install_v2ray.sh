fb6076fa5e107509b89939a5c3ba2e5784ccfd69
Mio Lau <liurusi.101@gmail.com>
2018-12-17 21:14:13 +0800

    [update] new v2ray config grammar;log to stdout and limit log size for v2ray container

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /etc/v2ray
 curl https://raw.githubusercontent.com/MioYvo/php_node_scripts/master/config.json -o /etc/v2ray/config.json
-docker run -itd --name v2ray --restart always -p 31560:31560 -p 31561:31561 -v /etc/v2ray/:/etc/v2ray/ v2ray/official
+docker run -itd --name v2ray --restart always -p 31560:31560 -p 31561:31561 -v /etc/v2ray/config.json:/etc/v2ray/config.json --log-opt max-size=10m --log-opt max-file=10 v2ray/official
