f06b15602e2accc3a64ea2666cc30cfd8b22cad6
Mio Lau <liurusi.101@gmail.com>
2019-01-24 07:13:45 +0800

    [update] add apache2.conf to add %V to apache2 log; use bigger log size 20m->50m

--- install.sh
+++ install.sh
@@ -14,4 +14,4 @@ systemctl enable docker
 # ------------------ start php node project ------------------
 docker stop mfpad
 docker rm mfpad
-docker run -itd --name mfpad -p 80:80 -e TZ=Asia/Shanghai --restart always --log-opt max-size=10m --log-opt max-file=10 mio101/php_node_scripts
+docker run -itd --name mfpad -p 80:80 -e TZ=Asia/Shanghai --restart always --log-opt max-size=50m --log-opt max-file=10 mio101/php_node_scripts
